9ab4904a450cdda97ca992ec5719cfd6c9528b10
wondongho <wondongho@mhworld.co.kr>
2023-06-22 08:39:43 +0900

    swagger config 추가

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,7 +5,19 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use OpenApi\Annotations as OA;
 
+/**
+ * @OA\Info(
+ *     title="Cat API Documentation",
+ *     version="1.0.0",
+ *     description="API Documentation for My Cat App",
+ *     @OA\License(
+ *         name="Apache 2.0",
+ *         url="https://www.apache.org/licenses/LICENSE-2.0.html"
+ *     )
+ * )
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
